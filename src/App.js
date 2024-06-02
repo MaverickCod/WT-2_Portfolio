f0495eac8964606b9b4f1947e679389dccf64570
MavericCod <921chavanprem@gmail.com>
2024-06-02 14:33:05 +0530

    comleted navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div >
-      <h1 className="sign">Hello</h1>
+    
+    <div> 
+      <Navbar/>
     </div>
   );
 }
