03153dbe2e26ce23cb001123d5b04cfa53dfcd45
MavericCod <921chavanprem@gmail.com>
2024-06-02 20:27:11 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './App.css';
+import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import SocialLinks from './components/SocialLinks';
@@ -8,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <About />
       <SocialLinks />
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className=" w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white ">
+        <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">About</p>
+            </div>
+
+            <p className="text-xl mt-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam corporis fugit aliquam voluptatum ut adipisci saepe nobis vel odio repudiandae sapiente voluptate, deleniti provident inventore fugiat. Voluptates facilis quis deleniti minus numquam? Similique nostrum molestiae accusamus aperiam, possimus ex! Debitis repudiandae, accusantium fugit eum id doloribus earum ad. Alias, minima?</p>
+
+            <br />
+            <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus error, nisi aspernatur placeat nulla similique perspiciatis dolorem odio aperiam at cupiditate recusandae delectus nemo voluptatem provident perferendis deleniti? Id ea dolor laboriosam voluptates hic deleniti ut, culpa debitis unde necessitatibus totam exercitationem quibusdam saepe ipsam recusandae quod tempore eos ab?</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
